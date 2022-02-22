164e1138973d134ca29119d51243583caf98b3e9
lysiandon <don.lysiane@gmail.com>
2022-02-22 14:16:30 +0100

    cleaned imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+//Views
 import Home from './components/Home';
 import Login from './components/Login';
 
